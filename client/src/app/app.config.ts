1a3fdf70f355d2049dd7bf8c00e649038e91e312
NCuongLinh <143623013+NCuongLinh@users.noreply.github.com>
2024-10-25 07:46:31 +0700

    add auth0

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideAuth0({
+      domain: 'dev-ujucu4328alcrthb.us.auth0.com',
+      clientId: 'SYvKw9lVlYRdOsmgyIgzsHI7LfrOz3cU',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),]
 };
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {LoginButtonComponent} from './components/login-button';
+import {LogoutButtonComponent} from './components/logout-button';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+  path: 'login',
+  component: LoginButtonComponent
+  },
+  {
+    path: 'logout',
+    component: LogoutButtonComponent
+  }
+];
