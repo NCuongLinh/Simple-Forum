6af3296248986e98239bb162859c536d24d3a24d
NCuongLinh <143623013+NCuongLinh@users.noreply.github.com>
2024-10-28 23:50:58 +0700

    Add section link

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {FooterComponent} from './components/footer/footer.component';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {HeaderComponent} from './components/header/header.component';
+import {SectionLinkComponent} from './components/section-link/section-link.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, NavbarComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent, HeaderComponent, SectionLinkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ client/src/app/components/section-link/section-link.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-link',
+  standalone: true,
+  imports: [],
+  templateUrl: './section-link.component.html',
+  styleUrl: './section-link.component.css'
+})
+export class SectionLinkComponent {
+
+}
